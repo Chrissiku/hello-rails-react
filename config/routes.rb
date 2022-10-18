d3aea86a0a5adbc3fa97f0d5d36026e3fbbbd869
Chris Siku <chrissiku5@gmail.com>
2022-10-18 15:16:45 +0200

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  
   root 'statics#index'
   get '/api/messages', to: 'statics#greeting'
-  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-message1 = Greeting.create(message: "Hello, world!")
-message2 = Greeting.create(message: "Hello, Rails!")
-message3 = Greeting.create(message: "Hello, Ruby!")
-message4 = Greeting.create(message: "Hello, JavaScript!")
-message5 = Greeting.create(message: "Hello, Python!")
+# frozen_string_literal: true
 
+message1 = Greeting.create(message: 'Hello, world!')
+message2 = Greeting.create(message: 'Hello, Rails!')
+message3 = Greeting.create(message: 'Hello, Ruby!')
+message4 = Greeting.create(message: 'Hello, JavaScript!')
+message5 = Greeting.create(message: 'Hello, Python!')
